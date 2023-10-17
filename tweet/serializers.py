b9f038090223831bf36d24432248df488489d586
Nima <nimiologyy@gmail.com>
2023-10-17 10:39:58 +0330

    tweet serializer

--- tweet/serializers.py
+++ tweet/serializers.py
@@ -1,12 +1,30 @@
 from rest_framework import serializers
 
 from tweet.models import Tweet
-from users.serializers import MyUserSerializer
+from tweet_like.models import TweetLike
+from users.serializers import FollowingUserSerializer
 
 
 class TweetSerializer(serializers.ModelSerializer):
-    owner = MyUserSerializer(read_only=True)
+    owner = FollowingUserSerializer(read_only=True)
+    user_like = serializers.SerializerMethodField()
 
     class Meta:
         model = Tweet
         fields = '__all__'
+
+    def _user(self):
+        request = self.context.get('request', None)
+        if request:
+            return request.user
+        return None
+
+    def get_user_like(self, obj):
+        user = self._user()
+        if user is not None:
+            if user.is_authenticated:
+                try:
+                    return TweetLike.objects.get(owner=user, tweet=obj).pk
+                except TweetLike.DoesNotExist:
+                    pass
+        return None
